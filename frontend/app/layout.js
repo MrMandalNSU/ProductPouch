a4475dae75d309ff2cdd5a3a9b22a4ce76699d91
Sudipta Mandal <sudipta.mandal@northsouth.edu>
2025-03-03 02:24:06 +0600

    Added mantine wrapper

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@mantine/core/styles.css";
+import MantineWrapper from "./MantineWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +21,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="antialiased">
+        <MantineWrapper>{children}</MantineWrapper>
       </body>
     </html>
   );
