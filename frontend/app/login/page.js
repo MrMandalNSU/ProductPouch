4a65bcec05ef52a0035d738aa52904f277716cbe
Sudipta Mandal <sudipta.mandal@northsouth.edu>
2025-03-02 22:10:35 +0600

    Adjusted design as per sketch

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -14,38 +14,42 @@ export default function Login() {
 
   return (
     <div className="flex h-screen justify-center items-center bg-gray-100">
-      <div className="bg-white p-10 rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-semibold text-center mb-6">SIGN IN</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full p-3 border border-gray-300 rounded mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 border border-gray-300 rounded mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button
-            type="submit"
-            className="w-full bg-indigo-500 hover:bg-indigo-600 text-white font-semibold p-3 rounded transition"
-          >
-            LOGIN
-          </button>
-        </form>
-        <p className="text-center text-gray-600 mt-4">
-          Don’t have an account?{" "}
-          <Link href="/signup" className="text-blue-500 font-semibold">
-            Signup
-          </Link>
-        </p>
+      <div className="relative flex flex-col items-center">
+        <h2 className="text-3xl font mb-4 text-black">SIGN IN</h2>
+
+        {/* Login Box */}
+        <div className="bg-white p-15 border border-gray-300 shadow-md w-100">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full p-3 border border-gray-300 rounded mb-4 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full p-3 border border-gray-300 rounded mb-6 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type="submit"
+              className="w-25 bg-indigo-500 hover:bg-indigo-600 text-white font-semibold p-3 rounded transition mx-auto block"
+            >
+              LOGIN
+            </button>
+          </form>
+          <p className="text-center text-gray-600 mt-4">
+            Don’t have an account?{" "}
+            <Link href="/signup" className="text-blue-500 font-semibold">
+              Signup
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
