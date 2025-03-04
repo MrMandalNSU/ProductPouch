92803c5e8820e693122e8f5ae2a520ef75e0dd1e
Sudipta Mandal <sudipta.mandal@northsouth.edu>
2025-03-04 10:43:38 +0600

    updated mutation with user login

--- frontend/graphql/mutations.js
+++ frontend/graphql/mutations.js
@@ -1,4 +1,3 @@
-// graphql/mutations.js
 import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
@@ -12,6 +11,20 @@ export const CREATE_USER = gql`
       phone
       createdAt
       updatedAt
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        first_name
+        last_name
+        email
+      }
     }
   }
 `;
--- frontend/lib/apollo-client.js
+++ frontend/lib/apollo-client.js
@@ -1,11 +1,25 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
 });
 
+// Auth link middleware
+const authLink = setContext((_, { headers }) => {
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   defaultOptions: {
     mutate: {
