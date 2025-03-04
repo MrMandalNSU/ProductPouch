47217b14903c2682bd725e0d078d99e026eaafe2
Sudipta Mandal <sudipta.mandal@northsouth.edu>
2025-03-04 11:20:56 +0600

    updated provider with auth context

--- frontend/app/providers.js
+++ frontend/app/providers.js
@@ -2,7 +2,12 @@
 
 import { ApolloProvider } from "@apollo/client";
 import client from "../lib/apollo-client";
+import { AuthProvider } from "../context/auth-context";
 
 export function Providers({ children }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <AuthProvider>{children}</AuthProvider>
+    </ApolloProvider>
+  );
 }
